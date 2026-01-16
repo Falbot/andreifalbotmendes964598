6b422f9bac89dc09fb6cf55a52dd53f79ec1bc77
Andrei Falbot Mendes <falbot@gmail.com>
2026-01-15 23:26:07 -0400

    fix(test): Ajustar AutoConfigureMockMvc para pacote do Spring Boot 4.

--- backend/src/test/java/br/com/falbot/seplag/backend/BackendApplicationTests.java
+++ backend/src/test/java/br/com/falbot/seplag/backend/BackendApplicationTests.java
@@ -1,13 +1,11 @@
 package br.com.falbot.seplag.backend;
 
+import br.com.falbot.seplag.backend.support.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BackendApplicationTests {
+class BackendApplicationTests extends IntegrationTestBase {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
---
+++ backend/src/test/java/br/com/falbot/seplag/backend/support/IntegrationTestBase.java
@@ -0,0 +1,34 @@
+package br.com.falbot.seplag.backend.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class IntegrationTestBase {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
+            .withDatabaseName("app")
+            .withUsername("app")
+            .withPassword("app");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+
+        // garante Flyway no mesmo banco do container
+        registry.add("spring.flyway.url", postgres::getJdbcUrl);
+        registry.add("spring.flyway.user", postgres::getUsername);
+        registry.add("spring.flyway.password", postgres::getPassword);
+    }
+}
